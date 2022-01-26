f171ceffa063f2cd77798883259b566102fe21c1
FLAVIO AUGUSTO BARRETO SILVA <95008802+flaviobarreto@users.noreply.github.com>
2022-01-26 19:22:21 -0300

    Jogo RPJ usando OO em C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RPJ.srs.Entities;
+
+namespace dotnet__poo{
+    class Program{
+        static void Main(string[]args){
+
+            Hero arus = new Hero("Arus", 23, "Knigth");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+
+            Console.WriteLine(arus.Attack());
+            Console.WriteLine(wizard.Attack());
+        }
+    }
+}
